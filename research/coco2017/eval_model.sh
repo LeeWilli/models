8ebf5f890af6ac81b2e1bec226a54601615c9751
LeeWilli <wangli@xinktech.com>
2018-03-29 10:30:19 +0800

    add file

---
+++ research/coco2017/eval_model.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SPLIT=validation  # or test
+MODEL=ssd_mobilenet_v1_coco_2017_11_08 #ssd_mobilenet_v1_coco_2018_1
+TF_RECORD_FILES=$(ls -1 ${SPLIT}_tfrecords/* | tr '\n' ',')
+PYTHONPATH=$PYTHONPATH:$(readlink -f ..) \
+python3 -m object_detection.inference.infer_detections \
+  --input_tfrecord_paths=$TF_RECORD_FILES \
+  --output_tfrecord_path=${MODEL}_${SPLIT}_detections.tfrecord-00000-of-00001 \
+  --inference_graph=${MODEL}/frozen_inference_graph.pb \
+  --discard_image_pixels
+
+NUM_SHARDS=1  # Set to NUM_GPUS if using the parallel evaluation script above
+mkdir -p ${MODEL}_${SPLIT}_eval_metrics
+echo "
+label_map_path: '../object_detection/data/mscoco_label_map.pbtxt'
+tf_record_input_reader: { input_path: '${MODEL}_${SPLIT}_detections.tfrecord@${NUM_SHARDS}'  }
+" > ${MODEL}_${SPLIT}_eval_metrics/${SPLIT}_input_config.pbtxt
+echo "
+metrics_set: 'open_images_metrics'
+" > ${MODEL}_${SPLIT}_eval_metrics/${SPLIT}_eval_config.pbtxt
+
+
+python3 -m  object_detection.metrics.offline_eval_map_corloc \
+  --eval_dir=${MODEL}_${SPLIT}_eval_metrics \
+  --eval_config_path=${MODEL}_${SPLIT}_eval_metrics/${SPLIT}_eval_config.pbtxt \
+  --input_config_path=${MODEL}_${SPLIT}_eval_metrics/${SPLIT}_input_config.pbtxt
+
+
